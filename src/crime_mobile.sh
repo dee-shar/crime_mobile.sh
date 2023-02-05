538eeaae1900a98d2754e3e2dd83a4a8a9bc4ec0
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-02-05 17:18:28 +0800

    Update crime_mobile.sh

--- src/crime_mobile.sh
+++ src/crime_mobile.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
 api="https://cdn.crime-mobile.ru"
+
 function get_api_info() {
 	curl --request GET \
 		--url "$api/api.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
 
 function get_servers_info() {
 	curl --request GET \
 		--url "$api/servers_info.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
 
 function get_story_info() {
 	curl --request GET \
 		--url "$api/story.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
 
 function get_files_info() {
 	curl --request GET \
 		--url "$api/files.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
 
 function get_online_info() {
 	curl --request GET \
 		--url "$api/monitoring/online.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
 
 function get_black_list() {
 	curl --request GET \
 		--url "$api/black_list.json" \
-		--header "user-agent: okhttp/3.14.9" \
-		--header "accept: application/json"
+		--user-agent "okhttp/3.14.9" \
+		--header "accept: application/json" \
+		--header "content-type: application/json"
 }
